f30b239b0613d848ca83cf57da99d179c68a8b7c
ahmadaziz6720 <a715210@gmail.com>
2023-02-24 12:38:09 +0700

    done 265

---
+++ modul 2/265/main.ino
@@ -0,0 +1,29 @@
+int ledList[8] = {2, 4, 5, 18, 19, 21, 22, 23};
+
+int i = 0;
+
+//led shift when button pressed
+void ledShift(){
+    delay(50);
+    digitalWrite(ledList[i], !digitalRead(ledList[i]));
+    i++;
+    if(i == 8){
+        i = 0;
+    }
+}
+
+
+void setup() {
+    // put your setup code here, to run once:
+    for (int i = 0; i < 8; i++){
+        pinMode(ledList[i], OUTPUT);
+    }
+
+    pinMode(15, INPUT_PULLUP);
+    attachInterrupt(15, ledShift, FALLING);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
+
